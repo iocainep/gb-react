1345336aa1b02a395ba21cb575d4bc78274a919f
iocainep <lonermine@gmail.com>
2021-10-11 11:37:42 +0300

    Добавлена логика ответа робота, thunk, redux-persist

--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -1,3 +1,5 @@
+import {AUTHORS} from "../../components/mock";
+
 export const ADD_MESSAGE = 'MESSAGES:ADD_MESSAGE'
 
 export const addMessage = (chatId, message) => ({
@@ -7,3 +9,21 @@ export const addMessage = (chatId, message) => ({
         message,
     },
 })
+
+export const sendMessageToBot = (chatId, message) => {
+    return (dispatch, getState) => {
+        dispatch(addMessage(chatId, message))
+
+        let timer = setTimeout(() => {
+            dispatch(
+                addMessage(chatId, {
+                    id: `message${Date.now()}`,
+                    author: AUTHORS.BOT,
+                    text: 'hello, I am bot',
+                })
+            )
+
+            clearTimeout(timer)
+        }, 2000)
+    }
+}
